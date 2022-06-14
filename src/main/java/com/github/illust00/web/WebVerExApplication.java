c1153a96774d07e1d2795bd6b43b6ae46a89aa54
johnny <jeonghan@gmail.com>
2022-06-14 16:49:15 +0900

    Initial commit from spring init

---
+++ src/main/java/com/github/illust00/web/WebVerExApplication.java
@@ -0,0 +1,13 @@
+package com.github.illust00.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVerExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebVerExApplication.class, args);
+	}
+
+}
